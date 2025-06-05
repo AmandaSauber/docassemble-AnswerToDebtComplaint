e445e707ad668caae7c4d65af6f28fe446cdca52
Amanda Sauber <21244564+StateSupport@users.noreply.github.com>
2025-06-05 12:43:48 -0500

    updates

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.AnswerToDebtComplaint',
       license='The MIT License',
       url='https://courtformsonline.org',
       packages=find_namespace_packages(),
-      install_requires=['docassemble.AssemblyLine>=3.2.0', 'docassemble.BrandingMN'],
+      install_requires=['docassemble.AssemblyLine @ git+https://github.com/SuffolkLITLab/docassemble-AssemblyLine.git@main'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/AnswerToDebtComplaint/', package='docassemble.AnswerToDebtComplaint'),
      )
